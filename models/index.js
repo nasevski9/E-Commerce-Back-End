25b969c00794fbf6fcc9abfee7095b927162b231
nasevski9 <140076217+nasevski9@users.noreply.github.com>
2023-09-20 19:07:05 +0200

    Update index.js

--- models/index.js
+++ models/index.js
@@ -6,11 +6,16 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: category_id,
+  onDelete: SET NULL,
+});
 
 // Categories have many Products
 
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: category_id
+});
 
 // Products belongToMany Tags (through ProductTag)
 
